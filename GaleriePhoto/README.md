71158d12e9c33601b3bd37d956f529a93282013f
kevinfrancomme <kevin_francomme@hotmail.fr>
2018-01-18 22:35:52 +0100

    Ajout Use case Formulaire inscription

--- GaleriePhoto/README.md
+++
@@ -1,4 +0,0 @@
-GaleriePhoto
-============
-
-A Symfony project created on December 5, 2017, 8:37 am.
--- GaleriePhoto/src/UPJV/GaleriePhotoBundle/UPJVGaleriePhotoBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace UPJV\GaleriePhotoBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class UPJVGaleriePhotoBundle extends Bundle
-{
-}
